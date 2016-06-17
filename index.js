60adda92eaddf5ee1a44c5db0578d7bf6ef95f37
chrisramsey <cramsey@helpsocial.com>
2016-06-17 12:56:48 -0500

    Adds ability to wait until after callback returns truthy

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 var map = require('map-stream'),
   _log = console.log;
 
+function runAfter(settings) {
+  if (typeof settings.after === 'function') {
+    _log('Wait: Calling after()');
+    settings.after();
+  }
+}
+
 module.exports = function (settings) {
 
   if (typeof settings !== 'object') {
-    settings = {
-      duration: settings
-    };
+
+    if (typeof settings === 'function') {
+      settings = {
+        callback: settings
+      };
+    } else {
+      settings = {
+        duration: settings
+      };
+    }
   }
 
   if (!settings.verbose) {
@@ -26,16 +40,33 @@ module.exports = function (settings) {
       settings.before();
     }
 
-    var timeout = setTimeout(function () {
-      timeout = null;
-      _log('Wait: Waited', settings.duration);
-      if (typeof settings.after === 'function') {
-        _log('Wait: Calling after()');
-        settings.after();
+    if (typeof settings.callback === 'function') {
+      var result = false;
+      var run_count = 0;
+      _log('Waiting for callback to return true');
+
+      while(true) {
+        run_count++;
+        result = settings.callback();
+
+        if (result) {
+          break;
+        }
       }
-      cb(null, file);
-    }, settings.duration);
 
+      _log('Callback has return truthy. Times called', run_count);
+
+      runAfter(settings);
+
+    } else {
+      var timeout = setTimeout(function () {
+        timeout = null;
+        _log('Wait: Waited', settings.duration);
+
+        runAfter(settings);
+
+        cb(null, file);
+      }, settings.duration);
+    }
   });
-
 };
